04b0ae52c04293c814be9a2de7111f34b7b3603f
prgrmz07 <prgrmz07@163.com>
2024-07-11 12:27:34 +0800

    [正常获得某提交的文件列表] 提交0的文件列表 == diff(commit0.tree, commit1.tree)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using LibGit2Sharp;
+using System.Text.Json;
 
 /*
 运行命令:
@@ -23,9 +24,19 @@ class Program
       {
         //获得 仓库提交列表
         List<LibGit2Sharp.Commit> commitLs=GetGitCommits(repoPtr);
-        // string commitLsJsonTxt=System.Text.Json.JsonSerializer.Serialize(commitLs[0]); //这句话会异常退出, 应该是libgit2sharp对libgit2封装不够完善
-        Console.WriteLine($"commitLs.Count={commitLs.Count}, commitLs[0]= {commitLs[0]} , commitLs[-1]= {commitLs[commitLs.Count-1]}  ");
-        // commitLs.Count=601, commitLs[0]= a1e7272fa856d38eb3d59ba15532e2280381419d , commitLs[-1]= 5681647c83fca0b18d0c8ba4c53ea4a48f57e35f  
+        LibGit2Sharp.Commit endCommit=commitLs[commitLs.Count-1];
+        LibGit2Sharp.Commit commit0=commitLs[0];
+        LibGit2Sharp.Commit commit1=commitLs[1];
+        Console.WriteLine($"commitLs.Count={commitLs.Count}, commitLs[0]= {commit0} , commitLs[1]= {commit1} , commitLs[-1]= {endCommit}  ");
+        // commitLs.Count=601, commitLs[0]= a1e7272fa856d38eb3d59ba15532e2280381419d , commitLs[1]= 53810ef8bf8140f19284eb85ea809f3ea8a7ca74 , commitLs[-1]= 5681647c83fca0b18d0c8ba4c53ea4a48f57e35f  
+      
+        //提交0的文件列表 == diff(commit0.tree, commit1.tree)
+        LibGit2Sharp.Tree tree0= GetFileListOfCommit(repoPtr, commit0.Sha);
+        LibGit2Sharp.Tree tree1= GetFileListOfCommit(repoPtr, commit1.Sha);
+        TreeChanges tree_diff = repoPtr.Diff.Compare<TreeChanges>(tree1, tree0);
+        string tree_diff__jsonTxt=JsonSerializer.Serialize(tree_diff);
+        Console.WriteLine($"tree_diff__jsonTxt={tree_diff__jsonTxt}");
+        // tree_diff__jsonTxt=[{"Path":"readme.md","Mode":33188,"Oid":{"RawId":"0J6Pk1ViaG9HGPZHDXRzFIzR+Nk=","Sha":"d09e8f935562686f4718f6470d7473148cd1f8d9"},"Exists":true,"Status":3,"OldPath":"readme.md","OldMode":33188,"OldOid":{"RawId":"XRb1gMw3H8nQK1QEsEZd3UetbVw=","Sha":"5d16f580cc371fc9d02b5404b0465ddd47ad6d5c"},"OldExists":true},{"Path":"work_list.md","Mode":33188,"Oid":{"RawId":"eLEL9P4rTB9Vga/TbR01FIETTBE=","Sha":"78b10bf4fe2b4c1f5581afd36d1d351481134c11"},"Exists":true,"Status":1,"OldPath":"work_list.md","OldMode":0,"OldOid":{"RawId":"AAAAAAAAAAAAAAAAAAAAAAAAAAA=","Sha":"0000000000000000000000000000000000000000"},"OldExists":false}]
       }
 
     }
@@ -53,6 +64,15 @@ class Program
   {
     return repoPtr.Commits.ToList();
   }
+
+  /*libgit2sharp获得 提交的文件列表
+  */
+  static LibGit2Sharp.Tree GetFileListOfCommit(Repository repoPtr ,string gitCommitHash)
+  {
+    Commit commit=repoPtr.Lookup<Commit>(gitCommitHash);
+    return commit.Tree;
+  }
+
 
 }//end_of_class
 }//end_of_namespace
