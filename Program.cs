64144ac17cc8b3bd9733ceed83da17515ac04099
prgrmz07 <prgrmz07@163.com>
2024-07-11 11:12:53 +0800

    libgit2sharp获得 仓库提交列表

--- Program.cs
+++ Program.cs
@@ -16,19 +16,43 @@ namespace CloneRepo
       string localRepoDir = "/tmp/gitReopTest01_for_libgit2";
 
       try{
-      cloneRepo(repoUrl,localRepoDir);
+        //克隆仓库
+        cloneRepo(repoUrl,localRepoDir);
+
+        //获得 仓库提交列表
+        List<LibGit2Sharp.Commit> commitLs=GetGitCommits(localRepoDir);
+        // string commitLsJsonTxt=System.Text.Json.JsonSerializer.Serialize(commitLs[0]); //这句话会异常退出, 应该是libgit2sharp对libgit2封装不够完善
+        Console.WriteLine($"commitLs.Count={commitLs.Count}, commitLs[0]= {commitLs[0]} , commitLs[-1]= {commitLs[commitLs.Count-1]}  ");
       }
       catch (Exception ex)
       {
         Console.WriteLine($"An error occurred: {ex.Message}");
       }
     }
+
+    /*libgit2sharp克隆仓库
+    libgit2sharp并没有带出libgit2的repoPtr, 说明 libgit2sharp 也是为了图省事, 但效率肯定不高, 因为每次都要libgit2重新打开仓库
+    */
     static void cloneRepo(string repoUrl,string localRepoDir)
     {
         // Clone the repository
         Console.WriteLine($"Cloning from '{repoUrl}' to '{localRepoDir}'...");
         Repository.Clone(repoUrl, localRepoDir);
         Console.WriteLine("Repository cloned successfully.");
+
+        
+    }
+
+    /*libgit2sharp获得 仓库提交列表
+    */
+    static List<LibGit2Sharp.Commit> GetGitCommits(string localRepoDir)
+    {
+      using (Repository repo = new Repository(localRepoDir))
+      {
+        return repo.Commits.ToList();
+      }
+
+
     }
   }
 }
