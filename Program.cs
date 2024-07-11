b0084262aa37fb9871a384ea70c763fdd893ab7c
prgrmz07 <prgrmz07@163.com>
2024-07-11 12:38:23 +0800

    [微调]  获得 仓库提交列表

--- Program.cs
+++ Program.cs
@@ -23,9 +23,12 @@ class Program
       {
         //获得 仓库提交列表
         List<LibGit2Sharp.Commit> commitLs=GetGitCommits(repoPtr);
-        // string commitLsJsonTxt=System.Text.Json.JsonSerializer.Serialize(commitLs[0]); //这句话会异常退出, 应该是libgit2sharp对libgit2封装不够完善
-        Console.WriteLine($"commitLs.Count={commitLs.Count}, commitLs[0]= {commitLs[0]} , commitLs[-1]= {commitLs[commitLs.Count-1]}  ");
-        // commitLs.Count=601, commitLs[0]= a1e7272fa856d38eb3d59ba15532e2280381419d , commitLs[-1]= 5681647c83fca0b18d0c8ba4c53ea4a48f57e35f  
+        LibGit2Sharp.Commit endCommit=commitLs[commitLs.Count-1];
+        LibGit2Sharp.Commit commit0=commitLs[0];
+        LibGit2Sharp.Commit commit1=commitLs[1];
+        Console.WriteLine($"commitLs.Count={commitLs.Count}, commitLs[0]= {commit0} , commitLs[1]= {commit1} , commitLs[-1]= {endCommit}  ");
+        // commitLs.Count=601, commitLs[0]= a1e7272fa856d38eb3d59ba15532e2280381419d , commitLs[1]= 53810ef8bf8140f19284eb85ea809f3ea8a7ca74 , commitLs[-1]= 5681647c83fca0b18d0c8ba4c53ea4a48f57e35f  
+      
       }
 
     }
@@ -53,6 +56,7 @@ class Program
   {
     return repoPtr.Commits.ToList();
   }
+
 
 }//end_of_class
 }//end_of_namespace
