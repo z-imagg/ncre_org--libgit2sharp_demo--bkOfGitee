ccc5196ed77c7ae4e1c2c18b949155acab54846e
prgrmz07 <prgrmz07@163.com>
2024-07-11 11:35:10 +0800

    [注释] libgit2sharp的` new Repository` 返回值 基本就代表了libgit2中的repoPtr

--- Program.cs
+++ Program.cs
@@ -18,11 +18,15 @@ class Program
     try{
       //克隆仓库
       cloneRepo(repoUrl,localRepoDir);
-      //获得 仓库提交列表
-      List<LibGit2Sharp.Commit> commitLs=GetGitCommits(localRepoDir);
-      // string commitLsJsonTxt=System.Text.Json.JsonSerializer.Serialize(commitLs[0]); //这句话会异常退出, 应该是libgit2sharp对libgit2封装不够完善
-      Console.WriteLine($"commitLs.Count={commitLs.Count}, commitLs[0]= {commitLs[0]} , commitLs[-1]= {commitLs[commitLs.Count-1]}  ");
-      // commitLs.Count=601, commitLs[0]= a1e7272fa856d38eb3d59ba15532e2280381419d , commitLs[-1]= 5681647c83fca0b18d0c8ba4c53ea4a48f57e35f  
+
+      using (Repository repoPtr = new Repository(localRepoDir))
+      {
+        //获得 仓库提交列表
+        List<LibGit2Sharp.Commit> commitLs=GetGitCommits(repoPtr);
+        // string commitLsJsonTxt=System.Text.Json.JsonSerializer.Serialize(commitLs[0]); //这句话会异常退出, 应该是libgit2sharp对libgit2封装不够完善
+        Console.WriteLine($"commitLs.Count={commitLs.Count}, commitLs[0]= {commitLs[0]} , commitLs[-1]= {commitLs[commitLs.Count-1]}  ");
+        // commitLs.Count=601, commitLs[0]= a1e7272fa856d38eb3d59ba15532e2280381419d , commitLs[-1]= 5681647c83fca0b18d0c8ba4c53ea4a48f57e35f  
+      }
 
     }
     catch (Exception ex)
@@ -32,7 +36,8 @@ class Program
   }
 
   /*libgit2sharp克隆仓库
-  libgit2sharp并没有带出libgit2的repoPtr, 说明 libgit2sharp 也是为了图省事, 但效率肯定不高, 因为每次都要libgit2重新打开仓库
+  libgit2sharp并没有带出libgit2的repoPtr, 
+  但是 libgit2sharp的` new Repository` 返回值 基本就代表了libgit2中的repoPtr  ， 以后用repoPtr进行各种操作即可
   */
   static void cloneRepo(string repoUrl,string localRepoDir)
   {
@@ -44,12 +49,9 @@ class Program
 
   /*libgit2sharp获得 仓库提交列表
   */
-  static List<LibGit2Sharp.Commit> GetGitCommits(string localRepoDir)
+  static List<LibGit2Sharp.Commit> GetGitCommits(Repository repoPtr )
   {
-    using (Repository repo = new Repository(localRepoDir))
-    {
-    return repo.Commits.ToList();
-    }
+    return repoPtr.Commits.ToList();
   }
 
 }//end_of_class
