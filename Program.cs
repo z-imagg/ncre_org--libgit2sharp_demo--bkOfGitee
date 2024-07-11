8e69e5d152ff140824a4f0e795a4fe6401d4874e
prgrmz07 <prgrmz07@163.com>
2024-07-11 10:37:17 +0800

    使用 LibGit2Sharp  克隆给定url的仓库 到本地目录
    使用 LibGit2Sharp  克隆仓库http://giteaz:3000/bal/cmd-wrap.git到本地目录 /tmp/gitReopTest01_for_libgit2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using LibGit2Sharp;
+
+/*
+运行命令:
+rm -fr /tmp/gitReopTest01_for_libgit2/ ; dotnet build && dotnet run
+*/
+
+namespace CloneRepo
+{
+  class Program
+  {
+
+    static void Main(string[] args){
+      string repoUrl = "http://giteaz:3000/bal/cmd-wrap.git";
+      string localRepoDir = "/tmp/gitReopTest01_for_libgit2";
+
+      try{
+      cloneRepo(repoUrl,localRepoDir);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"An error occurred: {ex.Message}");
+      }
+    }
+    static void cloneRepo(string repoUrl,string localRepoDir)
+    {
+        // Clone the repository
+        Console.WriteLine($"Cloning from '{repoUrl}' to '{localRepoDir}'...");
+        Repository.Clone(repoUrl, localRepoDir);
+        Console.WriteLine("Repository cloned successfully.");
+    }
+  }
+}
